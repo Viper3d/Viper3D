e7409f70c3a06a805204fbd3cbdcec64d667e1d1
Viper3d <150278499+Viper3d@users.noreply.github.com>
2023-11-09 00:46:14 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -58,9 +58,11 @@ app.get('/servicios', (req, res) => {
 
 
 // Iniciar el servidor en el puerto 3000
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
