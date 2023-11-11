f07e8984cd44dbeb910b2cb2bf048380e1ec3d26
Viper3d <150278499+Viper3d@users.noreply.github.com>
2023-11-11 17:23:53 +0100

    Add files via upload

--- multerConfig.js
+++ multerConfig.js
@@ -11,69 +11,10 @@ const client = new google.auth.JWT(
 
 const drive = google.drive({ version: 'v3', auth: client });
 
-// Verificar la conexión con Google Drive
-async function verifyDriveConnection() {
-  try {
-    await drive.files.list({
-      pageSize: 10,
-      fields: 'files(id, name)',
-    });
-    console.log("Conexión exitosa con Google Drive");
-  } catch (error) {
-    console.error("Error al conectar con Google Drive:", error);
-  }
-}
-
-
-
-verifyDriveConnection();
-
-const videoFolder = '13GpXRx0KOVynwvcU4RdFm35fnyYt4MrL'; // ID de la carpeta de videos en Drive
-const imgFolder = '1ldv5248POuRX4O39RF7snLksx8Uj7snh'; // ID de la carpeta de imágenes en Drive
-
-// Función para crear y subir un archivo txt a Google Drive
-async function uploadTextToDrive(text, folderId) {
-  // Convertir el texto a un Buffer
-  const textBuffer = Buffer.from(text, 'utf-8');
-
-  // Crear un objeto 'file' simulado que Multer podría haber creado
-  const file = {
-    originalname: 'test.txt', // El nombre que tendrá el archivo en Drive
-    mimetype: 'text/plain',
-    buffer: textBuffer,
-  };
-
-  // Usar la función 'uploadToDrive' para subir el archivo
-  try {
-    const fileId = await uploadToDrive(file, folderId);
-    console.log('Archivo txt subido con éxito, ID:', fileId);
-    return fileId; // Retorna el ID del archivo subido
-  } catch (error) {
-    console.error('Error al subir archivo txt a Google Drive:', error);
-    throw error; // Lanza el error para manejarlo más arriba si es necesario
-  }
-}
-
-// Ejemplo de uso de la función
-uploadTextToDrive('funciona', imgFolder) // Asegúrate de usar el ID de la carpeta correcta
-  .then((fileId) => {
-    // Haz algo con el fileId si es necesario
-  })
-  .catch((error) => {
-    // Manejo de errores
-  });
-
-// Configuración de Multer para almacenar archivos en memoria
-const storage = multer.memoryStorage();
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-};
-
-const upload = multer({ storage, fileFilter });
+// ID de la carpeta de videos en Drive
+const videoFolder = '13GpXRx0KOVynwvcU4RdFm35fnyYt4MrL';
+// ID de la carpeta de imágenes en Drive
+const imgFolder = '1ldv5248POuRX4O39RF7snLksx8Uj7snh';
 
 // Función para subir archivos a Google Drive
 const uploadToDrive = async (file, folderId) => {
@@ -93,6 +34,60 @@ const uploadToDrive = async (file, folderId) => {
   return response.data.id;
 };
 
+// Función para crear y subir un archivo txt a Google Drive
+async function uploadTextToDrive(text, folderId) {
+  const textBuffer = Buffer.from(text, 'utf-8');
+  const file = {
+    originalname: 'test.txt',
+    mimetype: 'text/plain',
+    buffer: textBuffer,
+  };
+  
+  try {
+    const fileId = await uploadToDrive(file, folderId);
+    console.log('Archivo txt subido con éxito, ID:', fileId);
+    return fileId;
+  } catch (error) {
+    console.error('Error al subir archivo txt a Google Drive:', error);
+    throw error;
+  }
+}
+
+// Configuración de Multer para almacenar archivos en memoria
+const storage = multer.memoryStorage();
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+// Verificar la conexión con Google Drive
+async function verifyDriveConnection() {
+  try {
+    await drive.files.list({
+      pageSize: 10,
+      fields: 'files(id, name)',
+    });
+    console.log("Conexión exitosa con Google Drive");
+  } catch (error) {
+    console.error("Error al conectar con Google Drive:", error);
+  }
+}
+
+// Llamar a las funciones
+verifyDriveConnection();
+uploadTextToDrive('funciona', imgFolder)
+  .then((fileId) => {
+    // Haz algo con el fileId si es necesario
+  })
+  .catch((error) => {
+    // Manejo de errores
+  });
+
 module.exports = {
   upload,
   uploadToDrive,
@@ -100,4 +95,3 @@ module.exports = {
   imgFolder,
   drive
 };
-
